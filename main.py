dd46b11cd1d66a9e6b855bee40462a8baa9a74a8
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2023-02-07 11:55:07 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Sintaxis General
+print("Hola Mundo")
+print('lA SUMA DE 1 MAS 2 ES: ', 1 + 2)
+if 5 > 2:
+    print('Cinco es mayor que 2')
+print('Termino')
+
+# Variables
+nombre = 'Marcos'
+edad = 20
+monto = 200.30
+print(nombre, edad)
+
+# Casting
+mensaje = nombre + "tiene " + str(edad) + " años"
+premio = monto + float(10)
+cantidad = edad + int(1.0)
+print(mensaje, premio, cantidad)
